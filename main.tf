8f0da69f617940ec96f44acfa6663b13d8552fcc
souta.nakamori <souta.nakamori@mixi.co.jp>
2024-08-29 15:18:53 +0900

    Add tag

--- main.tf
+++ main.tf
@@ -20,5 +20,7 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = "Handson_SoutaNakamori_2"
     TED = "20240829_Handson"
+    Environment = "development"
+    Project = "Handson_SoutaNakamori"
   }
 }
